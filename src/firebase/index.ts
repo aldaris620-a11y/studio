bc7a430cd4eb82f26492bf351bb368e60be6851c
Cesar Pocohuanca <aldaris620@gmail.com>
2025-11-13 23:25:51 +0000

    Try fixing this error: `Console Error: [2025-11-13T23:25:23.318Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,42 +2,29 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
+  const isConfigured = getApps().length > 0;
+  const app = !isConfigured ? initializeApp(firebaseConfig) : getApp();
+  const auth = getAuth(app);
+  const firestore = getFirestore(app);
 
-    return getSdks(firebaseApp);
+  if (process.env.NODE_ENV === 'development' && !auth.emulatorConfig && !firestore.emulatorConfig) {
+      // Connect to emulators if in development and not already connected
+      // NOTE: process.env.NEXT_PUBLIC_... variables are not available in this file
+      try {
+        connectAuthEmulator(auth, 'http://127.0.0.1:9099', { disableWarnings: true });
+        connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
+      } catch (e) {
+        console.error('Error connecting to Firebase emulators. Make sure they are running.', e);
+      }
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
-}
 
-export function getSdks(firebaseApp: FirebaseApp) {
-  return {
-    firebaseApp,
-    auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
-  };
+  return { firebaseApp: app, auth, firestore };
 }
 
 export * from './provider';
