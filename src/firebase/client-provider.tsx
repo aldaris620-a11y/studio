4e3ad901b204895c7533025e302a302114dfd301
Cesar Pocohuanca <aldaris620@gmail.com>
2025-11-13 23:46:11 +0000

    Ahora la página se queda cargando indefinidamente

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -7,6 +7,7 @@ import { initializeFirebase } from '@/firebase';
 import type { FirebaseApp } from 'firebase/app';
 import type { Auth } from 'firebase/auth';
 import type { Firestore } from 'firebase/firestore';
+import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
@@ -42,6 +43,7 @@ export function FirebaseClientProvider({ children }: FirebaseClientProviderProps
       auth={firebaseServices.auth}
       firestore={firebaseServices.firestore}
     >
+      <FirebaseErrorListener />
       {children}
     </FirebaseProvider>
   );
